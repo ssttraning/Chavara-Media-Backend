cdb8d2bb33b25ecf977b0c3cde0238247429e77c
ssttraning <ssttraning@gmail.com>
2026-06-10 16:00:57 +0530

    email_url_changed

--- Routes/approvetutor.js
+++ Routes/approvetutor.js
@@ -51,7 +51,7 @@ router.post('/', (req, res) => {
             });
 
             const createPasswordLink =
-                `http://localhost:5173/create-password/${token}`;
+                `http://chavaramedia.santhisoft.com/create-password/${token}`;
 
             try {
 
