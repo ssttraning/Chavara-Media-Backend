9065fb54922606865a11f7bdb7ff98780b6fb5cd
ssttraining <ssttraining@gmail.com>
2026-06-27 13:01:51 +0530

    page_correction

--- Routes/getcoursebycategory.js
+++ Routes/getcoursebycategory.js
@@ -8,7 +8,7 @@ router.post('/', (req, res) => {
     const categoryid = req.body.categoryid;
        const date = new Date().toISOString().split('T')[0];
 
-    const sql = `select * from tbl_course c inner join tbl_mode m on c.course_mode_id=m.mode_id inner join tbl_type t on c.course_type_id=t.type_id where c.course_category_id='${categoryid}' and course_lastdate >='${date}'`;
+    const sql = `select * from tbl_course c inner join tbl_mode m on c.course_mode_id=m.mode_id inner join tbl_type t on c.course_type_id=t.type_id where c.course_category_id='${categoryid}'`;
 // console.log(sql)
     db.query(sql, (err, result) => {
         if (err) {
