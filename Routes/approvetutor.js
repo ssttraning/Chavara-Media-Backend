006273665bd09a962ac63831689f236677178fbe
unknown <ssttraning@gmail.com>
2026-07-06 14:37:46 +0530

    email_testing6

--- Routes/approvetutor.js
+++ Routes/approvetutor.js
@@ -51,12 +51,14 @@ router.post('/', (req, res) => {
 
             const transporter = nodemailer.createTransport({
                 host: "smtp.gmail.com",
-                 port: 465,
-                 secure: true,
+                port: 465,
+                secure: true,
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS
-                }
+                },
+                connectionTimeout: 20000,
+                socketTimeout: 20000
             });
 
             const createPasswordLink =
