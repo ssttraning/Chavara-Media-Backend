aae577fa9947f2dbf75d1f3139e96f1477e06304
unknown <ssttraning@gmail.com>
2026-07-06 15:53:36 +0530

    email_testing9

--- Routes/approvetutor.js
+++ Routes/approvetutor.js
@@ -53,8 +53,8 @@ router.post('/', (req, res) => {
 
             const transporter = nodemailer.createTransport({
                 host: "smtp-relay.brevo.com",
-                port: 587,
-                secure: false,
+                port: 465,
+                secure: true,
                 auth: {
                     user: process.env.BREVO_USER,
                     pass: process.env.BREVO_PASS,
