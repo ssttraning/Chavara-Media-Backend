bd101d06cf02babacbdde8e28b50dbc3c4481fc1
unknown <ssttraning@gmail.com>
2026-07-07 10:03:37 +0530

    email_testing13

--- Routes/approvetutor.js
+++ Routes/approvetutor.js
@@ -51,8 +51,9 @@ router.post('/', (req, res) => {
 
             const transporter = nodemailer.createTransport({
                 host: "smtp.gmail.com",
-                port: 465,
-                secure: true,
+                port: 587,
+                secure: false,
+                requireTLS: true,
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS
