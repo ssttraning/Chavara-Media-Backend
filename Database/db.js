2e46932e81a797189f0f06902cacb26bf05ba5c7
unknown <ssttraning@gmail.com>
2026-07-13 10:49:51 +0530

    backend error solving test1

--- Database/db.js
+++ Database/db.js
@@ -1,8 +1,24 @@
 const mysql = require('mysql2');
 const db = mysql.createPool({
-    host:"n1nlmysql57plsk.secureserver.net",
-    user:"chavaramedia",
-    password:"b79Cmi49~",
-    database:"chavaramedia"
+    host: "n1nlmysql57plsk.secureserver.net",
+    port: 3306,
+    user: "chavaramedia",
+    password: "b79Cmi49~",
+    database: "chavaramedia",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 30000
 })
-module.exports=db;
+
+db.getConnection((err, conn) => {
+    if (err) {
+        console.error("MYSQL CONNECTION FAILED");
+        console.error(err);
+    } else {
+        console.log("MYSQL CONNECTED");
+        conn.release();
+    }
+});
+
+module.exports = db;
