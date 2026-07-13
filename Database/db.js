85fa2202973f2c443e8f7f7b7c35f3a803581fe3
unknown <ssttraning@gmail.com>
2026-07-13 12:34:43 +0530

    backend error solving test3

--- Database/db.js
+++ Database/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const db = mysql.createPool({
     host:"n1nlmysql57plsk.secureserver.net",
-    user:"chavaramedia",
-    password:"b79Cmi49~",
-    database:"chavaramedia"
+    user:"chavaramediaNew",
+    password:"wJz9@00j7",
+    database:"chavaramediaNew"
 })
 module.exports=db;
